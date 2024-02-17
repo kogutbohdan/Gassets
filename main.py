82a19b793e20cccb40058fd00afd355a514795ff
kogutbohdan <144838647+kogutbohdan@users.noreply.github.com>
2024-02-17 18:00:05 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,46 +1,64 @@
-class Gasset:
-    __articles={}
+class Text:
+    def __init__(self,text):
+        self.__text=text
+
+    def add(self,article):
+        article._addContent(self.__text)
+
+class Image:
+    def __init__(self,img):
+        self.__img=img
+
+    def add(self,article):
+        article._addContent(f"print {self.__img}")
+
+class ObjectInform:
     def __init__(self,caption):
         self.__caption=caption
+        self.__contents=[]
 
     @property
     def caption(self):
         return self.__caption
 
-    def addArticles(self,name,article):
-        self.__articles[name]=article
-
-    def removeArticle(self,name):
-        del self.__articles[name]
-
-    def useArticle(self,func):
-        for nameArticle in self.__articles:
-            print(self.__articles[nameArticle].caption)
-            self.__articles[nameArticle].use(func)
-
-class Printer:
-    def print(self,gasset):
-        print(gasset.caption)
-        gasset.useArticle(print)
-
-class Article:
-    __contents=[]
-    def __init__(self,caption):
-        self.__caption=caption
-
-    @property
-    def caption(self):
-        return self.__caption
-
-    def addContent(self,content):
+    def _addContent(self,content):
         self.__contents.append(content)
 
-    def use(self,func):
+    def _useContent(self,func):
         for content in self.__contents:
             func(content)
 
 
-gasset=Gasset("Вода")
+class Gasset(ObjectInform):
+    def addArticle(self,caption,*contents):
+        article=Article(caption)
+        for content in contents:
+            article.add(content)
+        self._addContent(article)
 
+    def useArticle(self,func):
+        self._useContent(func)
+
+
+class Printer:
+    def __printArticle(self,article):
+        print(article.caption)
+        article.use(print)
+
+    def print(self,gasset):
+        print(gasset.caption)
+        gasset.useArticle(self.__printArticle)
+
+class Article(ObjectInform):
+    def add(self,type):
+        type.add(self)
+
+    def use(self,func):
+        self._useContent(func)
+
+
+
+gasset=Gasset("Вода")
+gasset.addArticle("hdjfhkjdd", Image("img.png"),Text("j<shkdfjghshdfgakjdgfahldsgf"))
 printer=Printer()
 printer.print(gasset)
